4eb881742704b467b92222679efffb152ffeb53a
Oscar Junior <ojmjunior@gmail.com>
2020-11-25 16:34:13 -0300

    Stupid coach with some css - test is not working

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -15,5 +15,4 @@ class QuestionsController < ApplicationController
                         "I don't care, get dressed and go to work!"
                       end
   end
-  raise
 end
--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,10 +2,12 @@ require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   Capybara.register_driver(:headless_chrome) do |app|
+    client = Selenium::WebDriver::Remote::Http::Default.new
+    client.timeout = 120
     capabilities = Selenium::WebDriver::Remote::Capabilities.chrome \
       chromeOptions: { args: %w[headless disable-gpu window-size=1280x760] }
     Capybara::Selenium::Driver.new app,
-                                   browser: :chrome, desired_capabilities: capabilities
+                                   browser: :chrome, desired_capabilities: capabilities, http_client: client
   end
   driven_by :selenium, using: :headless_chrome
 end
